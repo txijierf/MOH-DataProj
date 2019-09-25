cf7322114c5664484a1ec4384f30121cbf912b28
Lemon <Alfred.Lemon@ontario.ca>
2019-09-25 08:55:45 -0400

    Refractor Packages

--- frontend/src/views/Package/Packages.js
+++ frontend/src/views/Package/Packages.js
@@ -1,18 +1,19 @@
-import React, {useEffect, useMemo} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import {
-  Paper, Grid, Button, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions,
-  Fade, Select, MenuItem
-} from '@material-ui/core';
-import {
-  adminGetPackages, userGetPackages, adminDeletePackage
-} from "../../controller/package";
-import {getCurrentUserOrganizations} from '../../controller/userManager';
+
+
+import React, { useState, useEffect, useMemo } from 'react';
+
+import { adminGetPackages, userGetPackages, adminDeletePackage } from "../../controller/package";
+import { getCurrentUserOrganizations } from '../../controller/userManager';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, Grid, Button, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Fade, Select, MenuItem } from '@material-ui/core';
 import PackageCard from './components/Card';
 import Loading from "../components/Loading";
 import PackagePicker from "./components/Picker";
 
-const useStyles = makeStyles(theme => ({
+import uniqid from "uniqid";
+
+const useStyles = makeStyles((theme) => ({
   container: {
     padding: theme.spacing(2),
     paddingLeft: theme.spacing(4),
@@ -20,173 +21,167 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function CreatePackage(props) {
-  const {params} = props;
+const Package = ({ fileName, deleteCb, handleOpen, openParams }) => (
+  <Grid key={uniqid()} item>
+    <PackageCard type="package" fileName={fileName} deleteCb={deleteCb} onOpen={handleOpen} openParams={openParams}/>
+  </Grid>
+);
+
+const DialogConfirmationMessage = ({ selectedName }) => (
+  <DialogContent>
+    <DialogContentText>
+      Are you sure you want to delete <strong>{selectedName}</strong>? 
+      <br/>
+      This process cannot be undone.
+    </DialogContentText>
+  </DialogContent>
+);
+
+const DialogButtons = ({ handleCloseDialog, handleConfirmDelete }) => (
+  <DialogActions>
+    <Button onClick={handleCloseDialog} color="primary">Cancel</Button>
+    <Button onClick={handleConfirmDelete} color="primary">Delete</Button>
+  </DialogActions>
+);
+
+const DialogWindow = ({ open, selectedName, handleCloseDialog, handleConfirmDelete }) => (
+  <Dialog open={open} keepMounted onClose={handleCloseDialog} >
+    <DialogTitle>Confirm delete?</DialogTitle>
+    <DialogConfirmationMessage selectedName={selectedName}/>
+    <DialogButtons handleConfirmDelete={handleConfirmDelete} handleCloseDialog={handleCloseDialog}/>
+  </Dialog>
+);
+
+const UserSelectedOrg = ({ selectedUserOrg, userOrganizations, handleChange }) => {
+  if(selectedUserOrg === "") {
+    return <Loading message="Loading your organizations..."/>;  
+  } else if(userOrganizations.length === 0) {
+    return <p>You don't belong to any organizations</p>;
+  } else {
+    const Menu = useMemo(() => (
+      userOrganizations.map((organization) => <MenuItem key={uniqid()} value={organization}>{organization}</MenuItem>
+    )), [ userOrganizations ]);
+  
+    const inputProps = { name: "Organizations" };
+    return <Select value={selectedUserOrg} onChange={handleChange} inputProps={inputProps}>{Menu}</Select>
+  }
+};
+
+const CreatePackage = ({ showMessage, history, params }) => {
   const isAdmin = params.mode === "admin";
+
   const classes = useStyles();
-  const [values, setValues] = React.useState({
-    packages: null,
-    dialog: false,
-    picker: null,
-    selectedName: null,
-    organizations: [], // organizations in the picker
-    pickedPackage: null,
-    userOrganizations: [], // organizations for the current user
-    selectedUserOrg: '',
-  });
+
+  const [ packages, setPackages ] = useState([]);
+  const [ openDialog, setOpenDialog ] = useState(false);
+  const [ picker, setPicker ] = useState(null);
+  const [ selectedName, setSelectedName ] = useState(null);
+  const [ organizations, setOrganizations ] = useState([]);
+  const [ pickedPackage, setPickedPackage ] = useState(null);
+  const [ userOrganizations, setUserOrganizations ] = useState([]);
+  const [ selectedUserOrg, setSelectedUserOrg ] = useState("");
+
+  const fetchAndPopulateAdminValues = () => adminGetPackages().then((adminPackages) => setPackages(adminPackages));
+
+  const fetchAndPopulateUserValues = async () => {
+    const userOrganizations = await getCurrentUserOrganizations();
+    const selectedUserOrg = userOrganizations[0];
+    const userPackages = await userGetPackages(selectedUserOrg);
+
+    setUserOrganizations(userOrganizations);
+    setSelectedUserOrg(selectedUserOrg);
+    setPackages(userPackages);
+  };
 
   useEffect(() => {
-    if (isAdmin) {
-      adminGetPackages().then(packages => setValues(values => ({...values, packages})));
-    } else if (!isAdmin) {
-      getCurrentUserOrganizations().then(organizations => {
-          setValues(values => ({
-            ...values,
-            userOrganizations: organizations,
-            selectedUserOrg: organizations[0]
-          }));
-          userGetPackages(organizations[0]).then(packages => setValues(values => ({...values, packages})));
-        }
-      );
+    isAdmin
+      ? fetchAndPopulateAdminValues()
+      : fetchAndPopulateUserValues();
+  }, [ isAdmin ]);
 
+  
+  const handleOpen = (name, pickedPackage) => (
+    ({ target }) => {
+      isAdmin
+        ? openPicker(pickedPackage, target)
+        : history.push('/packages/' + name + '/' + selectedUserOrg);
     }
-  }, [isAdmin]);
-
-  const allPackages = () => {
-    const list = [];
-    const {packages} = values;
-    if (packages == null) return <Loading message="Loading Packages..."/>;
-    for (let i = 0; i < packages.length; i++) {
-      const name = packages[i].name;
-      list.push(
-        <Grid key={i} item>
-          <PackageCard type="package" fileName={name} deleteCb={isAdmin ? openDialog : undefined}
-                       onOpen={onOpen} openParams={[packages[i]]}
-          />
-        </Grid>
-      )
-    }
-    return list;
+  );
+  
+  const handleCloseDialog = () => {
+    if(openDialog) setOpenDialog(false);
+  };
+  
+  const handleOpenDialog = (name) => {
+    if(!openDialog) setOpenDialog(true);
+    if(selectedName !== name) setSelectedName(name);
   };
 
-  const onOpen = (name, pack) => e => {
-    if (isAdmin) {
-      // props.history.push('/admin/packages/' + name);
-      openPicker(pack, e.target);
-    } else
-      props.history.push('/packages/' + name + '/' + values.selectedUserOrg);
-  };
+  const openPicker = (pickedPackage, anchorEl) => {
+    const organizations = pickedPackage.organizations.map(({ name }) => name);
 
-  const closeDialog = () => setValues(values => ({...values, dialog: false, selectedName: null}));
-  const closePicker = () => setValues(values => ({...values, picker: null}));
-  const openDialog = name => setValues(values => ({...values, dialog: true, selectedName: name}));
-
-  const openPicker = (pack, anchorEl) => {
-    const organizations = pack.organizations.map(org => org.name);
-    if (organizations.length === 0) {
-      console.log('No organizations');
-    } else if (organizations.length === 1) {
-      props.history.push('/admin/packages/view/' + pack.name + '/org/' + organizations[0]);
+    if(!organizations.length) {
+      console.log("No organizations");
+    } else if(organizations.length === 1) {
+      history.push('/admin/packages/view/' + pickedPackage.name + '/org/' + organizations[0]);
     } else {
-      setValues(values => ({...values, organizations, picker: anchorEl, pickedPackage: pack}))
+      setPicker(anchorEl);
+      setOrganizations(organizations);
+      setPickedPackage(pickedPackage);
     }
+  };
+
+  const closePicker = () => {
+    if(picker !== null) setPicker(null);
   };
 
   const handleConfirmDelete = () => {
-    const name = values.selectedName;
-    if (isAdmin) {
-      adminDeletePackage(name).then(res => {
-        closeDialog();
-        setValues(values => ({...values, packages: values.packages.filter(p => p.name !== name)}));
-        props.showMessage(res.message, 'success');
-      }).catch(e => props.showMessage(e.toString() + '\nDetails: ' + e.response.data.message, 'error'));
-
+    if(isAdmin) {
+      adminDeletePackage(selectedName)
+      .then(({ message }) => {
+          handleCloseDialog();
+          setPackages(packages.filter(({ name }) => selectedName !== name));
+          showMessage(message, "success");
+        })
+        .catch((error) => showMessage(`${error.toString()} \nDetails: ${error.response.data.message} error`));
     }
   };
 
-  const dialog = () => {
-    return (
-      <Dialog
-        open={values.dialog}
-        keepMounted
-        onClose={closeDialog}
-      >
-        <DialogTitle>
-          {"Confirm delete ?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Are you sure you want to delete <strong>{values.selectedName}</strong>? <br/>
-            This process cannot be undone.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeDialog} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleConfirmDelete} color="primary">
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-    )
+  const handlePickedPackage = (organization) => history.push('/admin/packages/view/' + pickedPackage.name + '/org/' + organization);
+  
+  const handleChangeUserOrg = ({ target: { value } }) => {
+    userGetPackages(value)
+      .then((packages) => {
+        setSelectedUserOrg(value);
+        setPackages(packages)
+      });
   };
 
-  const handlePick = (org) => {
-    props.history.push('/admin/packages/view/' + values.pickedPackage.name + '/org/' + org);
-  };
-
-  const menu = useMemo(() => {
-    const items = [];
-    for (const org of values.userOrganizations) {
-      items.push(<MenuItem key={org} value={org}>{org}</MenuItem>)
-    }
-    return items;
-  }, [values.userOrganizations]);
-
-  const handleChangeUserOrg = e => {
-    const value = e.target.value;
-    setValues(values => ({...values, selectedUserOrg: value}));
-    userGetPackages(value).then(packages => setValues(values => ({...values, packages})));
-  };
-
-  const renderSelectOrg = useMemo(() => {
-    if (values.selectedUserOrg === '') return <Loading message="Loading your organizations..."/>;
-    if (menu.length === 0) return 'You don\'t belong to any organizations.';
-    return (
-      <Select
-        value={values.selectedUserOrg}
-        onChange={handleChangeUserOrg}
-        inputProps={{
-          name: 'Organization',
-        }}
-      >
-        {menu}
-      </Select>
-    )
-  }, [values.selectedUserOrg, menu]);
+  const AllPackages = useMemo(() =>
+    packages.map((_package) => <Package key={uniqid()} deleteCb={isAdmin ? handleOpenDialog: undefined} fileName={_package.name} handleOpen={handleOpen} openParams={[_package]}/>
+  ), [ packages ])
 
   return (
     <Fade in>
       <Paper className={classes.container}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography variant="h6" gutterBottom>
-              All Packages
-            </Typography>
-            {isAdmin ? null : renderSelectOrg}
+            <Typography variant="h6" gutterBottom>All Packages</Typography>
+            {!isAdmin && <UserSelectedOrg selectedUserOrg={selectedUserOrg} userOrganizations={userOrganizations} handleChange={handleChangeUserOrg}/>}
           </Grid>
-          {allPackages()}
+          {AllPackages}
         </Grid>
-        <br/>
-        {dialog()}
+        <DialogWindow open={openDialog} selectedName={selectedName} handleCloseDialog={handleCloseDialog} handleConfirmDelete={handleConfirmDelete}/>
         <PackagePicker
-          anchorEl={values.picker}
+          anchorEl={picker}
           onClose={closePicker}
-          onSelect={handlePick}
-          options={values.organizations}
+          onSelect={handlePickedPackage}
+          options={organizations}
           title={'Pick an organization'}
         />
       </Paper>
-    </Fade>)
-}
+    </Fade>
+  );
+};
+
+export default CreatePackage;
